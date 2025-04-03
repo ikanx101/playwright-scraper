12bd43b1a3ee5b996d074b5a86ae58b0bdcac627
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2025-04-04 06:14:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ module.exports = {
     // Default scraper options
     defaultOptions: {
         outputDir: 'saved_pages',
-        timeout: 30000,
-        retryCount: 3,
-        retryDelay: 5,
-        concurrency: 3,
+        timeout: 40000,
+        retryCount: 5,
+        retryDelay: 6,
+        concurrency: 8,
         headless: false
     },
     
