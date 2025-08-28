013fb31bcdd7e23130e2c3c469674fe472996fc0
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2025-08-28 11:18:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,10 +14,7 @@ module.exports = {
     
     // URLs to scrape
     urls: [
-<<<<<<< HEAD
-	"https://shopee.co.id/SiS-GO-Energy-Gel-Isotonic-60ml-Bstores-i.106955054.4522546469?sp_atk=6a4c3e5c-7794-42a5-b20e-fc08846a34ae&xptdk=6a4c3e5c-7794-42a5-b20e-fc08846a34ae"
-=======
+	"https://shopee.co.id/SiS-GO-Energy-Gel-Isotonic-60ml-Bstores-i.106955054.4522546469?sp_atk=6a4c3e5c-7794-42a5-b20e-fc08846a34ae&xptdk=6a4c3e5c-7794-42a5-b20e-fc08846a34ae",
 	'https://www.tokopedia.com/torch-id/product'
->>>>>>> 9adcbd22b1626f0a8c0e5f48922222808275d3cc
     ]
 }; 
